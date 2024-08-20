7572d4a18522545eafcc10c12c4b49911841d3ce
GHOSTFACE <erickkatana4787@gmail.com>
2024-08-20 20:16:45 +0300

    final updated files

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,35 +1,11 @@
-// src/components/QuestionList.js
-import React, { useState, useEffect } from 'react';
+import React from "react";
 
 function QuestionList() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:4000/questions')
-      .then(response => response.json())
-      .then(data => setQuestions(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  const deleteQuestion = (id) => {
-    fetch(`http://localhost:4000/questions/${id}`, {
-      method: 'DELETE',
-    })
-      .then(() => {
-        setQuestions(questions.filter(question => question.id !== id));
-      })
-      .catch(error => console.error('Error deleting question:', error));
-  };
-
   return (
-    <div>
-      {questions.map(question => (
-        <div key={question.id}>
-          <h2>{question.prompt}</h2>
-          <button onClick={() => deleteQuestion(question.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
+    <section>
+      <h1>Quiz Questions</h1>
+      <ul>{/* display QuestionItem components here after fetching */}</ul>
+    </section>
   );
 }
 
